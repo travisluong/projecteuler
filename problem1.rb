94cd588374a12a30f1b6d1bcf3710df538e8ebf9
Travis Luong <travis@travisluong.com>
2015-01-26 21:27:14 -0800

    solve problem 1

--- problem1.rb
+++ problem1.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 
 class Problem1
-  def self.sum_of_multiples_of_3_and_5_below num
+  def self.sum_of_multiples_of_3_and_5 num
     sum = 0
     (1..num-1).each do |i|
       if i%5 == 0 || i%3 == 0
@@ -12,9 +12,9 @@ class Problem1
   end
 end
 
-class Project1Test < MiniTest::Unit::TestCase
-  def test_sum_of_multiples_of_3_and_5_below_num
-    result = Problem1.sum_of_multiples_of_3_and_5_below 10
+class Problem1Test < MiniTest::Unit::TestCase
+  def test_sum_of_multiples_of_3_and_5
+    result = Problem1.sum_of_multiples_of_3_and_5 10
     assert_equal 23, result
   end
 end
